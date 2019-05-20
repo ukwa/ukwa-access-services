49c7f04c3646b977d730ba43e72a18480c86c446
Andrew Jackson <Andrew.Jackson@bl.uk>
2019-05-20 21:22:30 +0100

    Deployment config prior to updating IP list.

--- beta/docker-beta.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Pull in configuration environment variables:
-#source /etc/sysconfig/ukwa_access
-
-# Launch with correct combined configuration:
-docker-compose -f ../docker-compose.yml -f docker-compose.beta.yml -p ukwa_access_beta "$@"
