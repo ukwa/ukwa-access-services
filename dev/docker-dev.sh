efb767fca50241ca16cb3b3499908b52ea1607bf
Andrew Jackson <Andrew.Jackson@bl.uk>
2019-07-10 10:43:19 +0100

    Added sketch prototype for React UI.

--- dev/docker-dev.sh
+++ dev/docker-dev.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-export INTERNAL_IP=0.0.0.0
-
-# Launch with correct combined configuration:
-docker-compose -f ../docker-compose.yml -f docker-compose.dev.yml -p ukwa_access_dev "$@"
+# Launch with correct combined configuration
+docker stack deploy -c docker-compose.dev.yml access_dev
